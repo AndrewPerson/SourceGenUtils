02983095c96dd4d25f36df367e3ece94467fe05f
AndrewPerson <professor_smart@hotmail.com>
2025-10-15 12:18:11 +1100

    Moved to a newer C# version and added ability to create record structs

--- StringyType.cs
+++ StringyType.cs
@@ -2,46 +2,39 @@
 using System.Linq;
 using Microsoft.CodeAnalysis;
 
-namespace SourceGenUtils
+namespace SourceGenUtils;
+
+public readonly record struct StringyType
+(
+    string? Namespace,
+    string Type,
+    StringyType[] GenericParams
+)
 {
-    public readonly struct StringyType : IEquatable<StringyType>
+    public string NamespaceQualifiedType => Namespace == null ? $"global::{GenericType}" : $"global::{Namespace}.{GenericType}";
+    public string GenericType => GenericParams.Length == 0 ? Type : $"{Type}<{string.Join(", ", GenericParams.Select(p => p.NamespaceQualifiedType))}>";
+
+    public StringyType(ITypeSymbol type) : this
+    (
+        type.ContainingNamespace.IsGlobalNamespace ? null : type.ContainingNamespace.ToString(),
+        type.Name,
+        type is INamedTypeSymbol namedType
+            ? namedType.TypeArguments.Select(t => new StringyType(t)).ToArray()
+            : []
+    )
+    { }
+
+    public override string ToString() => NamespaceQualifiedType;
+
+    public bool Equals(StringyType other)
     {
-        public string? Namespace { get; }
-        public string Type { get; }
-        public StringyType[] GenericParams { get; }
+        return Namespace == other.Namespace
+               && Type == other.Type
+               && GenericParams.SequenceEqual(other.GenericParams);
+    }
 
-        public string NamespaceQualifiedType => Namespace == null ? $"global::{GenericType}" : $"global::{Namespace}.{GenericType}";
-        public string GenericType => GenericParams.Length == 0 ? Type : $"{Type}<{string.Join(", ", GenericParams.Select(p => p.NamespaceQualifiedType))}>";
-
-        public StringyType(string? @namespace, string type, StringyType[] genericParams)
-        {
-            Namespace = @namespace;
-            Type = type;
-            GenericParams = genericParams;
-        }
-
-        public StringyType(ITypeSymbol type) : this
-        (
-            type.ContainingNamespace.IsGlobalNamespace ? null : type.ContainingNamespace.ToString(),
-            type.Name,
-            type is INamedTypeSymbol namedType
-                ? namedType.TypeArguments.Select(t => new StringyType(t)).ToArray()
-                : new StringyType[0]
-        )
-        { }
-
-        public override string ToString() => NamespaceQualifiedType;
-
-        public bool Equals(StringyType other)
-        {
-            return Namespace == other.Namespace
-                && Type == other.Type
-                && Enumerable.SequenceEqual(GenericParams, other.GenericParams);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Namespace, Type, GenericParams.Aggregate(0, HashCode.Combine));
-        }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Namespace, Type, GenericParams.Aggregate(0, HashCode.Combine));
     }
 }
