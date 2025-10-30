f9c2045101de6bb174a0724520fcae24dcba056c
AndrewPerson <professor_smart@hotmail.com>
2025-10-30 17:31:13 +1100

    Added LocationTagged value comparer

--- LocationTagged.cs
+++ LocationTagged.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 
 namespace SourceGenUtils;
@@ -12,4 +13,19 @@ public readonly record struct LocationTagged<T>(
     public LocationTagged(T value, SyntaxReference syntaxRef) : this(value, new StringyLocation(syntaxRef)) { }
 
     public LocationTagged<U> Select<U>(Func<T, U> selector) => new(selector(Value), Location);
+
+    public class ValueComparer(IEqualityComparer<T>? inner) : IEqualityComparer<LocationTagged<T>>
+    {
+        public IEqualityComparer<T> Inner { get; } = inner ?? EqualityComparer<T>.Default;
+        
+        public bool Equals(LocationTagged<T> x, LocationTagged<T> y)
+        {
+            return Inner.Equals(x.Value, y.Value);
+        }
+
+        public int GetHashCode(LocationTagged<T> obj)
+        {
+            return Inner.GetHashCode(obj.Value);
+        }
+    }
 }
